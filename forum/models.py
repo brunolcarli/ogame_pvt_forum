0a582b0b9c414aada972f245feb1eb13b3ee96ba
Bruno L. Carli <brunolcarli@gmail.com>
2022-11-27 22:43:51 -0300

    ADD CustomUser model

--- forum/models.py
+++ forum/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class CustomUser(User):
+    avatar = models.BinaryField(null=True)
+    bio = models.TextField()
+
+
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posted_by')
     content = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)
     thread = models.ForeignKey('forum.Thread', on_delete=models.CASCADE)
@@ -12,18 +17,15 @@ class Post(models.Model):
 class Thread(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
     content = models.TextField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='thread_creator')
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='thread_creator')
     open_date = models.DateTimeField(auto_now_add=True)
     closed = models.BooleanField(default=False)
     closing_date = models.DateTimeField(null=True)
-    closed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin_user')
+    closed_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='admin_user', null=True)
     section = models.ForeignKey('forum.Section', on_delete=models.CASCADE)
+    last_post_datetime = models.DateTimeField(null=True)
 
 
 class Section(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     description = models.CharField(max_length=255, blank=False, null=False)
-
-
-
-
