5fa68313f61ec610ffe29c555aee72643a2905b4
Bruno L. Carli <brunolcarli@gmail.com>
2022-11-27 22:51:25 -0300

    CHANGE split settings

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 migrate:
-	python manage.py makemigrations
-	python manage.py migrate
+	python manage.py makemigrations --settings=ogame_forum_api.settings.${ENV_REF}
+	python manage.py migrate --settings=ogame_forum_api.settings.${ENV_REF}
 
 shell:
-	python manage.py shell
+	python manage.py shell --settings=ogame_forum_api.settings.${ENV_REF}
 
 
 run:
-	python manage.py runserver 0.0.0.0:9099
+	python manage.py runserver 0.0.0.0:9099 --settings=ogame_forum_api.settings.${ENV_REF}
---
+++ ogame_forum_api/settings/production.py
@@ -0,0 +1,20 @@
+import os
+from ogame_forum_api.settings.development import *
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': os.environ.get('MYSQL_DATABASE', ''),
+        'USER': os.environ.get('MYSQL_USER', ''),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', ''),
+        'HOST': os.environ.get('MYSQL_HOST', ''),
+        'PORT': os.environ.get('MYSQL_PORT'),
+        'OPTIONS': {
+          'autocommit': True,
+        }
+    }
+}
