01a2fd0cd0c134d83924eadefb0bf8114841642e
Bruno Luvizotto Carli <bruno.carli@smartgreen.net>
2022-12-02 11:39:37 -0300

    CHANGE version

--- forum/schema.py
+++ forum/schema.py
@@ -63,7 +63,7 @@ class Query:
     version = graphene.String()
 
     def resolve_version(self, info, **kwargs):
-        return '0.0.4'
+        return '0.0.5'
 
     #######################
     #  Multiple objects
